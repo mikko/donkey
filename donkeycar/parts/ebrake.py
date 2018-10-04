49a5b120949830f9ee21086c14d669c49ca668de
Mikko Pohja <mikko.pohja@futurice.com>
2018-10-04 22:58:22 +0300

    Circular buffer with median calculation for filtering noise in time to impact

--- donkeycar/parts/ebrake.py
+++ donkeycar/parts/ebrake.py
@@ -1,12 +1,20 @@
+from collections import deque
+from statistics import median
+
 class EBrake:
 
   def __init__(self):
+    self.buffer = deque(maxlen=3)
     print('Emergency brake started')
 
   def run(self, time_to_impact, raw_throttle):
-    if (time_to_impact > 0 and time_to_impact < 1):
+    self.buffer.append(time_to_impact)
+    # Use median from buffered values to avoid noise
+    filtered_time = median(list(self.buffer))
+
+    if (filtered_time > 0 and filtered_time < 1):
       print('\n\nEmergency brake used!!\n\n')
-      return 0, True
+      return -0.2, True
     return raw_throttle, False
 
   def shutdown(self):
